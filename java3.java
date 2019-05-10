879aef65244dcc5f36317bb32caff28df73fb6ba
wudongzi <2328901049@qq.com>
2019-05-10 23:24:42 +0800

    java练习1-hello 666

---
+++ java3.java
@@ -0,0 +1,17 @@
+public class java3{
+    String name;//名字
+    int price;//价格
+    public static void main(String[] args){
+        java3 changjian = new java3();
+        changjian.name = "长剑";
+        changjian.price = 350;
+
+        java3 xueping = new java3();
+        xueping.name = "血瓶";
+        xueping.price = 50;
+
+        java3 caoxie = new java3();
+        caoxie.name = "草鞋";
+        caoxie.price = 300;
+    }
+}
