2924df2b7fe073c094841753f09b1828eb186e51
wudongzi <2328901049@qq.com>
2019-05-10 23:15:44 +0800

    java练习1-hello 666

---
+++ java2.java
@@ -0,0 +1,19 @@
+public class java2{
+    String name;//名字
+    float hp;//血量
+    float armor;//护甲
+    int movespeed;//移动速度
+    public static void main(String[] args) {
+        java2 garen = new java2();
+        garen.name = "盖伦";
+        garen.hp = 110.28f;
+        garen.armor = 50.26f;
+        garen.movespeed = 350;
+
+        java2 teemo = new java2();
+        teemo.name = "提莫";
+        teemo.hp = 80.38f;
+        teemo.armor = 30f;
+        teemo.movespeed = 380;
+    }
+}
