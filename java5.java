1e342b2ed19be2f3b9a6f94f0f16ca0a3b6caa35
wudongzi <2328901049@qq.com>
2019-05-12 16:56:25 +0800

    java练习1-hello 666

---
+++ java5.java
@@ -0,0 +1,11 @@
+public class java5 {
+    public static void main(String[] args){
+        double a = 3.14;
+        float b = 2.769343f;
+        int c = 365;
+        byte d = 12;
+        char e = '吃';
+        boolean f = false;
+        String g = "不可描述";
+    }
+}
