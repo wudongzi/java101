bb906cc3c636c0217891565d1e3739f7dc4ef66c
wudongzi <2328901049@qq.com>
2019-05-20 20:12:51 +0800

    Java的逻辑操作符

---
+++ java14.java
@@ -0,0 +1,26 @@
+public class java14 {
+    public static void main(String[] args){
+        int i = 2;
+        System.out.println(i==1 & i++ == 2);
+        System.out.println(i);
+        int j = 3;
+        System.out.println(j==1 && ++j ==2);
+        System.out.println(j);
+
+        int a = 2;
+        System.out.println( a== 1 | a++ ==2  );
+        System.out.println(a)
+        int b = 2;
+        System.out.println( b== 2 || b++ ==2  );
+        System.out.println(b);
+
+        boolean c = true;
+        System.out.println(c);
+        System.out.println(!c);
+
+        boolean d = false
+        System.out.println(c^d);
+        System.out.println(c^!d);
+
+    }
+}
