261d482512f814833a7431976b90d5a6c40db669
wudongzi <2328901049@qq.com>
2019-05-21 19:43:27 +0800

    Java的赋值操作符

---
+++ java16.java
@@ -0,0 +1,16 @@
+public class java16 {
+    public static void main(String[] args){
+        int i = 5+5;
+        i+= 5;
+        System.out.println(i);
+
+        int j = 2;
+        j += 3;
+        System.out.println(j);
+
+        int t = 4;
+        t += t++;
+        System.out.println(t);
+
+    }
+}
