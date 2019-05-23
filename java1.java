3228b99feb109dd61ba52b08a02a2912df765336
wudongzi <2328901049@qq.com>
2019-05-23 22:53:45 +0800

    for语句

--- java1.java
+++ java1.java
@@ -1,5 +1,7 @@
 public class java1{
     public static void main(String[] args){
         System.out.println("hello 666");
+        int i = 5;
+        System.out.println(i^0);
     }
 }
---
+++ java22.java
@@ -0,0 +1,21 @@
+public class java22 {
+    public static void main(String[] args){
+        int i = 0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+
+        for(int j = 0;j < 5;j++){
+            System.out.println(j);
+        }
+
+        int b = 1;
+        int c = 2;
+        for(int a = 1;a < 11;a++){
+            System.out.println(b);
+            b = b*c;
+
+        }
+    }
+}
