be93611dd57e5e4ca5a99606425b2a0799d9b918
wudongzi <2328901049@qq.com>
2019-05-25 22:37:14 +0800

    继承

---
+++ java37.java
@@ -0,0 +1,33 @@
+public class java37{
+    String name;
+    int price;
+}
+/*
+public class Weapon extends {
+    int damage; //攻击力
+
+    public static void main(String[] args) {
+        Weapon infinityEdge = new Weapon();
+        infinityEdge.damage = 65; //damage属性在类Weapon中新设计的
+
+        infinityEdge.name = "无尽之刃";//name属性，是从Item中继承来的，就不需要重复设计了
+        infinityEdge.price = 3600;
+    }
+}
+public class Armor extends Item{
+
+    int ac; //护甲等级
+
+    public static void main(String[] args) {
+        Armor cloth = new Armor();
+        cloth.name="布甲";
+        cloth.price=300;
+        cloth.ac = 15;
+
+        Armor chainMail = new Armor();
+        chainMail.name="锁子甲";
+        chainMail.price=500;
+        chainMail.ac = 40;
+    }
+
+}*/
