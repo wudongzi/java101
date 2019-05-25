3e2d81b0754d3fb98c13ff82b04add869694f9ef
wudongzi <2328901049@qq.com>
2019-05-25 21:51:39 +0800

    引用

---
+++ java36.java
@@ -0,0 +1,20 @@
+public class java36 {
+    String name;
+    float hp;
+    float ammor;
+    int moveSpeed;
+    public static void main(String[] args) {
+        //java36 h1 = new java36();
+        //java36 h2 = h1;
+        //java36 h3 = h1;
+        //java36 h4 = h1;
+        //java36 h5 = h1;
+        java36 garen =  new java36();
+        java36 h1 =  new java36();
+        java36 h2 =  new java36();
+        java36 h3;
+        java36 h4;
+        h3 = h1;
+        h4 = h3;
+    }
+}
