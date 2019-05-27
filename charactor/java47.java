1f414993d37119c020d87ff218be3c0dda847e08
wudongzi <2328901049@qq.com>
2019-05-27 19:12:48 +0800

    枚举

---
+++ charactor/java47.java
@@ -0,0 +1,20 @@
+package charactor;
+public class java47 {
+    public static void main(String[] args) {
+        int season = 5;
+        switch (season) {
+            case 1:
+                System.out.println("春天");
+                break;
+            case 2:
+                System.out.println("夏天");
+                break;
+            case 3:
+                System.out.println("秋天");
+                break;
+            case 4:
+                System.out.println("冬天");
+                break;
+        }
+    }
+}
