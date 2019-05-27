b62aee2a10a93e08f4ffd1229752f144a74f2f4d
wudongzi <2328901049@qq.com>
2019-05-27 20:14:36 +0800

    覆盖1

---
+++ charactor/java51.java
@@ -0,0 +1,12 @@
+package charactor;
+
+public class java51 {
+    String name;
+    int price;
+    public void buy(){
+        System.out.println("购买");
+    }
+    public void effect() {
+        System.out.println("物品使用后，可以有效果");
+    }
+}
