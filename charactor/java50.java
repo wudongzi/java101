634f55f1df5d0a9b5285a7ed6f74702544e26a3a
wudongzi <2328901049@qq.com>
2019-05-27 19:35:53 +0800

    接口与继承

---
+++ charactor/java50.java
@@ -0,0 +1,18 @@
+package charactor;
+import charactor1.java42;
+
+public class java50 {
+    public String name;
+    protected float hp;
+    public static void main(String[] args) {
+        java50 ad = new java50();
+        java50 h = new java50();
+        h = ad;
+        java42 s =new java42();
+        ad = (java50) h;
+        ad = (java50)h;
+        java50 ap = new java50();
+        java50 h1= ad;
+        java50 h2= ap;
+    }
+}
