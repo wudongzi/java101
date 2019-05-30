a582821552383405dc5a5d758b6c9028873f10f7
wudongzi <2328901049@qq.com>
2019-05-30 21:30:50 +0800

    质数

---
+++ digit/TestNumber4.java
@@ -0,0 +1,26 @@
+package digit;
+
+public class TestNumber4 {
+
+    public static void main(String[] args) {
+
+        int max = 10000*1000;
+        int count = 0 ;
+        for (int i = 1; i <=max; i++) {
+            if(isPrime(i)){
+                count++;
+            }
+        }
+        System.out.println("一千万以内的质数一共有 : " + count);
+
+    }
+
+    private static boolean isPrime(int i) {
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if(0==i%j)
+                return false;
+        }
+        return true;
+    }
+
+}
