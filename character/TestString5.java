6a053f97b9373bea8d1a558ad68b3b35beff8a5a
wudongzi <2328901049@qq.com>
2019-05-31 22:07:41 +0800

    分隔

---
+++ character/TestString5.java
@@ -0,0 +1,13 @@
+package character;
+
+public class TestString5 {
+
+    public static void main(String[] args) {
+
+        String sentence = "        盖伦,在进行了连续8次击杀后,获得了 超神 的称号      ";
+
+        System.out.println(sentence);
+
+        System.out.println(sentence.trim());
+    }
+}
