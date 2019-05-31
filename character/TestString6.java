7035212f613d2b0bc768a4fec76b77b814f92ca2
wudongzi <2328901049@qq.com>
2019-05-31 22:09:19 +0800

    大小写

---
+++ character/TestString6.java
@@ -0,0 +1,14 @@
+package character;
+
+public class TestString6 {
+
+    public static void main(String[] args) {
+
+        String sentence = "Garen";
+
+        System.out.println(sentence.toLowerCase());
+
+        System.out.println(sentence.toUpperCase());
+
+    }
+}
