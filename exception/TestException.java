28d5d58224d3aff675ecef6f77456f53de575f8e
wudongzi <2328901049@qq.com>
2019-06-04 22:20:04 +0800

    try catch

--- exception/TestException.java
+++ exception/TestException.java
@@ -7,7 +7,9 @@ public class TestException {
 
     public static void main(String[] args) {
 
-        File f= new File("d:/LOL.exe");
+        //File f= new File("d:/LOL.exe");
 
         //试图打开文件LOL.exe，会抛出FileNotFoundException，如果不处理该异常，就会有编译错误
-        new FileInputStream(f);
+        //new FileInputStream(f);
+    }
+}
