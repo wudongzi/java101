bda4c0ed8b24cc1deceba0bcf56b79917865e9cf
wudongzi <2328901049@qq.com>
2019-06-05 21:51:24 +0800

    以字节流的形式读取文件内容

---
+++ stream/TestStream1.java
@@ -0,0 +1,32 @@
+package stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class TestStream1 {
+
+    public static void main(String[] args) {
+        try {
+
+            File f =new File("d:/lol.txt");
+
+            FileInputStream fis =new FileInputStream(f);
+
+            byte[] all =new byte[(int) f.length()];
+
+            fis.read(all);
+            for (byte b : all) {
+
+                System.out.println(b);
+            }
+
+            fis.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
