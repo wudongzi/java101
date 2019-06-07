1890765c4db5412f02bb615e48154d7d212b3bf7
wudongzi <2328901049@qq.com>
2019-06-07 22:59:09 +0800

    Scanner读取字符串

---
+++ stream/TestStream18.java
@@ -0,0 +1,17 @@
+package stream;
+
+import java.util.Scanner;
+
+public class TestStream18 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            String line = s.nextLine();
+            System.out.println(line);
+        }
+
+    }
+}
