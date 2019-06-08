f997f66031874bf5e1b60c5afd5839e5e98053b9
wudongzi <2328901049@qq.com>
2019-06-08 22:20:13 +0800

    没有顺序

---
+++ collection/TestCollection7.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.HashSet;
+
+public class TestCollection7 {
+    public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<Integer>();
+
+        numbers.add(9);
+        numbers.add(5);
+        numbers.add(1);
+
+
+        System.out.println(numbers);
+
+    }
+}
