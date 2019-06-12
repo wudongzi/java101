148173641872bf61fd441bb888c2a20930935966
wudongzi <2328901049@qq.com>
2019-06-12 18:41:08 +0800

    泛型的简写

---
+++ collection/TestCollection27.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import charactor.Hero1;
+
+public class TestCollection {
+
+    public static void main(String[] args) {
+        List<Hero1> genericheros = new ArrayList<Hero1>();
+        List<Hero1> genericheros2 = new ArrayList<>();
+
+    }
+
+}
